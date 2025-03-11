4cfc82df31166c850c5a629184f26ba87104e5a0
Sahana <82923085+sahana-github@users.noreply.github.com>
2025-03-11 21:45:41 +0530

    Update findlargestelement.py

--- findlargestelement.py
+++ findlargestelement.py
@@ -4,3 +4,22 @@ def findelement(arr):
 if __name__=="__main__":
   arr1=[2,4,5,10]
   print("The largest element in array is",findelement(arr))
+
+
+#recurssive approch
+
+# def findelement(arr,n):
+#   max=arr[0]
+#   for i in range(0,n):
+#     if max<arr[i]:
+#       max=arr[i]
+#   return max
+# if __name__=="__main__":
+#   arr1=[2,8,90]
+#   n=3
+#   max=findelement(arr1,n)
+#   print("largest element is",max)
+      
+    
+  
+  
