c55cca9b540e725bf3b6a2e33568aeaa20b609f2
Sahana <82923085+sahana-github@users.noreply.github.com>
2025-03-11 21:19:37 +0530

    Update gcd-bruteforce.py

--- gcd-bruteforce.py
+++ gcd-bruteforce.py
@@ -1,7 +1,11 @@
-def gcd(n1,n2):
+def find_gcd(n1,n2):
+  #initialise gcd to 1
   gcd=1
+  # iterate from 1 to min of n1 and n2
   for i in range(1,min(n1,n2)+1):
+    #check if its common factor
     if n1%i==0 and n2%i==0:
+      #if yes then update to gcd variable
       gcd=i
     return gcd
 
