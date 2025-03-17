f995ad90a6fa4692d821b9e76f45a0c9ce142a20
Sahana <82923085+sahana-github@users.noreply.github.com>
2025-03-17 19:18:47 +0530

    Update insertnewnode.py

--- insertnewnode.py
+++ insertnewnode.py
@@ -1,14 +1,27 @@
-def Linkedlist(self,data1,next1=None):
-  self.data=data1
-  self.next=next1
-def insertnode(head,val):
-  new_node=Linkedlist(val)
-  new_node.next=head
-  return new_node
-
-def print(head):
-  while head:
-    print(head,val, end="->")
-    head=head.next
-  print("None")
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
+class Linkedlist:
+  def __init__(self):
+    self.head=None
+  def insert(self,data):
+    new_node=Node(data)
+    if not in self.head:
+      self.head=new_node
+      return
+    temp=self.head
+    while temp.next:
+      temp=temp.next
+    temp.next=new_node
+  def display(self):
+    temp=self.head
+    while temp:
+      print(temp.data,end="->")
+      temp=temp.next
+    print("None")
+l1=Linkedist()
+l1.insert(10)
+l1.insert(20)
+l1.display()
     
