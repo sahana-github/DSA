0498d6864856aea7f48f0079d8a4a83c56c8614f
Sahana <82923085+sahana-github@users.noreply.github.com>
2025-03-18 06:28:04 +0530

    Create reverselinkedlist.py

---
+++ reverselinkedlist.py
@@ -0,0 +1,13 @@
+class ListNode:
+  def __init__(self,val=0,next=None):
+    self.val=val
+    self.next=next
+class solution:
+  def reverselist(self,head:Optional[ListNode]) -> Optional[ListNode]:
+    curr,prev=head,None
+    while curr:
+      temp=curr.next
+      curr.next=prev
+      prev=curr
+      curr=temp
+    return prev
